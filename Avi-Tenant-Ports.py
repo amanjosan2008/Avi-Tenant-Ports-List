d8716a6fdc637ce68648a0bdb1e5e63c5a075f8b
Amandeep Singh <31513195+amanjosan2008@users.noreply.github.com>
2019-11-07 17:00:24 +0530

    Update Avi-Tenant-Ports.py

--- Avi-Tenant-Ports.py
+++ Avi-Tenant-Ports.py
@@ -30,16 +30,20 @@ for i in config['Tenant']:
                     try:
                         pool_group = (re.findall(r'name=.+', config["VirtualService"][j]["pool_group_ref"]))[0].strip('name=').split('&')[0]
                         print(" - Pool Group: ", pool_group)
-                        for n in range(len(config["Pool"])):
-                            if (config["Pool"][n]["name"]) == pool:
-                                try:
-                                    for o in range(len(config["Pool"][n]["servers"])):
-                                        try:
-                                            print("   - ", config["Pool"][n]["servers"][o]['port'])
-                                        except KeyError:
-                                            print("   - ", config["Pool"][n]["default_server_port"])
-                                except KeyError:
-                                    print('   - No Servers')
-                    except KeyError:
+                        for n in range(len(config["PoolGroup"])):
+                            if (config["PoolGroup"][n]["name"]) == pool_group:
+                                for p in range(len(config["PoolGroup"][n]["members"])):
+                                    for q in range(len(config["Pool"])):
+                                        if (config["PoolGroup"][n]["members"][p]["pool_ref"].split('name=')[-1]).split('&')[0] == config["Pool"][q]["name"]:
+                                            try:
+                                                for o in range(len(config["Pool"][q]["servers"])):
+                                                    try:
+                                                        print("   - ", config["Pool"][q]["servers"][o]['port'])
+                                                    except KeyError:
+                                                        print("   - ", config["Pool"][q]["default_server_port"])
+                                            except KeyError:
+                                                print('   - No Servers')
+                    except:
+                        #print("   - No Pool Group")
                         pass
             print("\n")
